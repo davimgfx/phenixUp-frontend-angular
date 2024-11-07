62e6a1b609ca11d84bcb1f9357982142243fea85
Davi Fernandes <davifncosta@gmail.com>
2024-11-06 23:25:06 -0300

    feat: working with routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from "./components/button/button.component";
-import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PhenixUp - Better than ClickUp';
